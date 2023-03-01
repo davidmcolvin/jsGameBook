c1946b511d13e2d6673689843cbfde9f84cf82da
davidmcolvin <davidmcolvin@gmail.com>
2023-02-28 21:02:51 -0500

    Actually get chapter4 up to speed

--- chapter4/drawing.js
+++ chapter4/drawing.js
@@ -1,27 +1,41 @@
 function draw_grid(ctx, minor, major, stroke, fill) {
- minor = minor || 10;
- major = major || minor * 5;
- stroke = stroke || "#00FF00";
- fill = fill || "#009900";
- ctx.save();
- ctx.strokeStyle = stroke;
- ctx.fillStyle = fill;
- let width = ctx.canvas.width, height = ctx.canvas.height
- for(var x = 0; x < width; x += minor) {
- ctx.beginPath();
- ctx.moveTo(x, 0);
- ctx.lineTo(x, height);
- ctx.lineWidth = (x % major == 0) ? 0.5 : 0.25;
- ctx.stroke();
- if(x % major == 0 ) {ctx.fillText(x, x, 10);}
- }
- for(var y = 0; y < height; y += minor) {
- ctx.beginPath();
- ctx.moveTo(0, y);
- ctx.lineTo(width, y);
- ctx.lineWidth = (y % major == 0) ? 0.5 : 0.25;
- ctx.stroke();
- if(y % major == 0 ) {ctx.fillText(y, 0, y + 10);}
- }
- ctx.restore();
-}  
+    minor = minor || 10;
+    major = major || minor * 5;
+    stroke = stroke || "#00FF00";
+    fill = fill || "#009900";
+    ctx.save();
+    ctx.strokeStyle = stroke;
+    ctx.fillStyle = fill;
+    let width = ctx.canvas.width, height = ctx.canvas.height
+    for(var x = 0; x < width; x += minor) {
+    ctx.beginPath();
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, height);
+    ctx.lineWidth = (x % major == 0) ? 0.5 : 0.25;
+    ctx.stroke();
+    if(x % major == 0 ) {ctx.fillText(x, x, 10);}
+    }
+    for(var y = 0; y < height; y += minor) {
+    ctx.beginPath();
+    ctx.moveTo(0, y);
+    ctx.lineTo(width, y);
+    ctx.lineWidth = (y % major == 0) ? 0.5 : 0.25;
+    ctx.stroke();
+    if(y % major == 0 ) {ctx.fillText(y, 0, y + 10);}
+    }
+    ctx.restore();
+   }  
+   
+   function draw_pacman(ctx, x, y, r, o, stroke, fill ) {
+       stroke = stroke || "#00FF00";
+       fill = fill || "#009900";
+       ctx.save()
+       ctx.strokeStyle = stroke;
+       ctx.fillStyle = fill;
+       context.beginPath();
+       context.arc(x, y, r, (0.2 - (0.2 * o)) * Math.PI, (1.8 + (.2 * o)) * Math.PI);
+       context.lineTo(x, y);
+       context.closePath();
+       context.fill();
+       context.stroke();
+   }   
