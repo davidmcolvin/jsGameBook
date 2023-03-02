c7fecdc903cf6ab789e28ea948a2b2d1b389d305
davidmcolvin <davidmcolvin@gmail.com>
2023-03-01 21:53:09 -0500

    Start drawing a ship

--- chapter4/drawing.js
+++ chapter4/drawing.js
@@ -39,3 +39,37 @@ function draw_grid(ctx, minor, major, stroke, fill) {
        context.fill();
        context.stroke();
    }   
+   function draw_ship(ctx, x, y, radius, options) {
+    options = options || {};
+    ctx.save();
+    // optionally draw a guide showing the collision radius
+    if(options.guide) {
+    ctx.strokeStyle = "white";
+    ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+    ctx.lineWidth = 0.5;
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
+    }
+    // set some default values
+    ctx.lineWidth = options.lineWidth || 2;
+    ctx.strokeStyle = options.stroke || "white";
+    ctx.fillStyle = options.fill || "black";
+    let angle = (options.angle || 0.5 * Math.PI) / 2;
+    // draw the ship in three lines
+    ctx.beginPath();
+    ctx.moveTo(x + radius, y);
+    ctx.lineTo(
+    x + Math.cos(Math.PI - angle) * radius,
+    y + Math.sin(Math.PI - angle) * radius
+    );
+    ctx.lineTo(
+    x + Math.cos(Math.PI + angle) * radius,
+    y + Math.sin(Math.PI + angle) * radius
+    );
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+    ctx.restore();
+   }
